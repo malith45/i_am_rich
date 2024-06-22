a62f2bc8f172a891b54dcda42c6c2fa213e1e2dd
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2024-06-22 20:05:52 +0530

    Hello World !
    Starting from scratch.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//This is the main function of this project.
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
